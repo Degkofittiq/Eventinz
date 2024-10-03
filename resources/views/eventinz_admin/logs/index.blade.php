d16d282bcfe9f0751946fcfb4cc1a7360c022de4
Archange DEGUENON <Degkofittiq@ittiqdevv29@outlook.com>
2024-10-03 11:32:00 +0100

    Data table on admins logs list

--- resources/views/eventinz_admin/logs/index.blade.php
+++ resources/views/eventinz_admin/logs/index.blade.php
@@ -1,4 +1,34 @@
 @extends('eventinz_admin.layouts.app')
+<link rel="stylesheet" href="https://cdn.datatables.net/2.1.5/css/dataTables.dataTables.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.datatables.net/2.1.5/js/dataTables.min.js"></script>
+<style>
+    #dt-length-0{
+      margin: 5px !important;
+    }
+    .dt-length label{
+      text-transform: uppercase;
+    }
+    #padding{
+      padding: 5px !important;
+      width: 100%;
+    }
+    @media (max-width: 768px) {
+      #responsive {
+        max-width: 100%;
+        overflow-x: auto; /* Permet le défilement horizontal */
+        -webkit-overflow-scrolling: touch; /* Pour un défilement fluide sur mobile */
+      }
+      #responsive .dt-layout-row{
+        width: 100% !important;
+      }
+      
+      table {
+        width: 100%; /* S'assure que le tableau occupe tout l'espace disponible */
+        table-layout: auto; /* Ajuste automatiquement la largeur des colonnes */
+      }
+    }
+</style>
 @section('content_admin') 
 <div class="card card-primary">
   @if(session('success'))
@@ -24,7 +54,7 @@
     <!-- /.card-header -->
     <div class="card" id="responsive">
 
-        <table class="table">
+        <table class="table" id="myTable">
             <thead>
                 <tr>
                     <th>User</th>
@@ -37,7 +67,9 @@
             <tbody>
                 @foreach($logs->makeHidden(['email','password','_token']) as $log)
                     <tr>
-                        <td>{{ $log->user->name ?? 'Guest' }}</td>
+                        <td>
+                            <a href="{{ $log->user ? route('admin.edit.adminuserform',$log->user_id) : '#' }}" style="{{ $log->user ? 'color: black; text-decoration:underline' : 'color: black; text-decoration:underline' }}">{{ $log->user->username ?? 'Guest' }}</a>
+                        </td>
                         <td>{{ str_replace('admin/', "",  $log->path ) }}</td>
                         <td>{{ $log->method }}</td>
                         <td>
@@ -82,4 +114,14 @@
 
 </div>
 
+
+<script>
+    let table = new DataTable('#myTable', {
+      responsive: true, // Ajoute la réactivité
+    //   columnDefs: [
+    //     { orderable: false, targets: [7] } // 7 est l'index de la colonne 'Actions', car les index commencent à 0
+    //   ]
+    }
+    );
+  </script>
 @endsection
